c66146d855292f5fa663612b5ab10e29c32bb4e0
tazkir <tazkirfurqan77@gmail.com>
2022-04-11 16:16:47 +0800

    setup frontend dashborad
    Signed-off-by: tazkir <tazkirfurqan77@gmail.com>

--- dashboard/src/Redux/Actions/userActions.js
+++ dashboard/src/Redux/Actions/userActions.js
@@ -1,15 +1,12 @@
 import {
-  USER_LIST_FAIL,
-  USER_LIST_REQUEST,
   USER_LIST_RESET,
-  USER_LIST_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
-} from "../Constants/UserContants";
-import axios from "axios";
-import { toast } from "react-toastify";
+} from '../Constants/UserContants';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 // LOGIN
 export const login = (email, password) => async (dispatch) => {
@@ -24,7 +21,7 @@ export const login = (email, password) => async (dispatch) => {
 
     const config = {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     };
 
@@ -35,7 +32,7 @@ export const login = (email, password) => async (dispatch) => {
     );
 
     if (!data.isAdmin === true) {
-      toast.error("You are not Admin", ToastObjects);
+      toast.error('You are not Admin', ToastObjects);
       dispatch({
         type: USER_LOGIN_FAIL,
       });
@@ -43,13 +40,13 @@ export const login = (email, password) => async (dispatch) => {
       dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
     }
 
-    localStorage.setItem("userInfo", JSON.stringify(data));
+    localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
     const message =
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message;
-    if (message === "Not authorized, token failed") {
+    if (message === 'Not authorized, token failed') {
       dispatch(logout());
     }
     dispatch({
@@ -61,40 +58,6 @@ export const login = (email, password) => async (dispatch) => {
 
 // LOGOUT
 export const logout = () => (dispatch) => {
-  localStorage.removeItem("userInfo");
+  localStorage.removeItem('userInfo');
   dispatch({ type: USER_LOGOUT });
-  dispatch({ type: USER_LIST_RESET });
-};
-
-// ALL USER
-export const listUser = () => async (dispatch, getState) => {
-  try {
-    dispatch({ type: USER_LIST_REQUEST });
-
-    const {
-      userLogin: { userInfo },
-    } = getState();
-
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.token}`,
-      },
-    };
-
-    const { data } = await axios.get(`/api/users`, config);
-
-    dispatch({ type: USER_LIST_SUCCESS, payload: data });
-  } catch (error) {
-    const message =
-      error.response && error.response.data.message
-        ? error.response.data.message
-        : error.message;
-    if (message === "Not authorized, token failed") {
-      dispatch(logout());
-    }
-    dispatch({
-      type: USER_LIST_FAIL,
-      payload: message,
-    });
-  }
 };
--- dashboard/src/Redux/store.js
+++ dashboard/src/Redux/store.js
@@ -1,40 +1,19 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { userListReducer, userLoginReducer } from "./Reducers/userReducers";
-import {
-  productCreateReducer,
-  productDeleteReducer,
-  productEditReducer,
-  productListReducer,
-  productUpdateReducer,
-} from "./Reducers/ProductReducers";
-import {
-  orderDeliveredReducer,
-  orderDetailsReducer,
-  orderListReducer,
-} from "./Reducers/OrderReducres";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import { userLoginReducer } from "./Reducers/userReducers";
 
 const reducer = combineReducers({
-  userLogin: userLoginReducer,
-  userList: userListReducer,
-  productList: productListReducer,
-  productDelete: productDeleteReducer,
-  productCreate: productCreateReducer,
-  productEdit: productEditReducer,
-  productUpdate: productUpdateReducer,
-  orderList: orderListReducer,
-  orderDetails: orderDetailsReducer,
-  orderDeliver: orderDeliveredReducer,
+  // userLogin: userLoginReducer,
 });
 
 // login
-const userInfoFromLocalStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : null;
+// const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+//   ? JSON.parse(localStorage.getItem("userInfo"))
+//   : null;
 
 const initialState = {
-  userLogin: { userInfo: userInfoFromLocalStorage },
+  // userLogin: { userInfo: userInfoFromLocalStorage },
 };
 
 const middleware = [thunk];
