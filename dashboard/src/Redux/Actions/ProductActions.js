f4064c0859b0b0ad12523269adb768513642ccd1
tazkir <tazkirfurqan77@gmail.com>
2022-04-12 14:57:20 +0800

    admin create product
    Signed-off-by: tazkir <tazkirfurqan77@gmail.com>

--- dashboard/src/Redux/Actions/ProductActions.js
+++ dashboard/src/Redux/Actions/ProductActions.js
@@ -1,7 +1,7 @@
 import {
-  // PRODUCT_CREATE_FAIL,
-  // PRODUCT_CREATE_REQUEST,
-  // PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
@@ -83,3 +83,42 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     });
   }
 };
+
+// CREATE PRODUCT
+export const createProduct =
+  (name, price, description, image, countInStock) =>
+  async (dispatch, getState) => {
+    try {
+      dispatch({ type: PRODUCT_CREATE_REQUEST });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.post(
+        `/api/products/`,
+        { name, price, description, image, countInStock },
+        config
+      );
+
+      dispatch({ type: PRODUCT_CREATE_SUCCESS, payload: data });
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout());
+      }
+      dispatch({
+        type: PRODUCT_CREATE_FAIL,
+        payload: message,
+      });
+    }
+  };
